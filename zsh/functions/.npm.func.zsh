92e70ad483d6b5d4c589bede99222a0cf7cb2a83
Bruno Barbosa <bruno@barbosa.io>
2016-08-10 23:17:31 -0700

    feat(add eslint) add global eslint config

--- zsh/functions/.npm.func.zsh
+++ zsh/functions/.npm.func.zsh
@@ -3,5 +3,5 @@
 # installs airbnb style guide  packages & copy default eslintrc config to current directory
 function nil() {
 	npm install --save-dev eslint-config-airbnb eslint-plugin-import eslint-plugin-react eslint-plugin-jsx-a11y eslint
-	mv  /Users/bruno/.eslintrc.global "${pwd}/.eslintrc"
+	cp ~/.dotfiles/bin/configs/.eslintrc.global "${pwd}/.eslintrc"
 }
