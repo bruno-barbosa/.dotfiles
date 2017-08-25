5fff0d88ad9aef83fbc957c225f63b93c442b230
Bruno Barbosa <bruno@Chaos.local>
2017-08-24 23:12:25 -0700

    code refactor and add tmmuxinator

--- bin/setup/setup.sh
+++ bin/setup/setup.sh
@@ -3,6 +3,6 @@ export BIN_PATH=$HOME/.dotfiles/bin/setup
 
 source ${BIN_PATH}/bot.sh
 source ${BIN_PATH}/brew/brew.sh
-source ${BIN_PATH}/rvm.sh
+source ${BIN_PATH}/rvm/rvm.sh
 source ${BIN_PATH}/nvm.sh
 source ${BIN_PATH}/git.sh
--- zsh/bin/.spotify.zsh
+++ zsh/bin/.spotify.zsh
@@ -1,4 +1,4 @@
-prompt_zshShowStatus() {
+prompt_zshSpotify() {
   state=`osascript -e 'tell application "Spotify" to player state as string'`;
   if [ $state = "playing" ]; then
     artist=`osascript -e 'tell application "Spotify" to artist of current track as string'`;
